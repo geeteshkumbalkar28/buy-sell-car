092ca107a877b681cfa3d970b9a3de69edefd7e8
Ashutosh7796 <132451174+Ashutosh7796@users.noreply.github.com>
2023-06-28 12:10:27 +0530

    ADDED: Cors Config Update

--- JWT-with-Spring-Security/src/main/java/com/spring/jwt/config/filter/CorsFilter.java
+++ JWT-with-Spring-Security/src/main/java/com/spring/jwt/config/filter/CorsFilter.java
@@ -13,17 +13,25 @@ public class CorsFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
 
-        final HttpServletResponse httpServletResponse = (HttpServletResponse) response;
-
-        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
-        httpServletResponse.setHeader("Access-Control-Allow-Methods", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         httpServletResponse.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
-        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(((HttpServletRequest) request).getMethod())) {
+        if (httpServletRequest.getMethod().equalsIgnoreCase("OPTIONS")) {
             httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(request, response);
         }
-        chain.doFilter(request, response);
+    }
 
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
     }
 }
