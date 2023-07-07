463b69118c947f51f86f55fee62f0ddd5835ba1d
geeteshkumbalkar028 <geeteshkumbalkar@gmail.com>
2023-07-07 18:40:41 +0530

    Added : there is on permitAll userProfilePhoto

--- JWT-with-Spring-Security/src/main/java/com/spring/jwt/config/AppConfig.java
+++ JWT-with-Spring-Security/src/main/java/com/spring/jwt/config/AppConfig.java
@@ -80,6 +80,7 @@ public class AppConfig {
                 .authorizeHttpRequests()
                 .requestMatchers("/account/**").permitAll()
                 .requestMatchers("/cars/**").permitAll()
+                .requestMatchers("/userProfilePhoto/**").permitAll()
                 .requestMatchers("/admin/**").hasAuthority("ADMIN")
                 .requestMatchers("/photo/**").hasAnyAuthority("ADMIN", "DEALER")
                 .requestMatchers("/dealerDocument/**").hasAnyAuthority("ADMIN", "DEALER")
