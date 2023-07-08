c2ce7bf12c1df1bdf33a71bdbdf245bfbfae5709
Ashutosh7796 <132451174+Ashutosh7796@users.noreply.github.com>
2023-07-08 14:20:11 +0530

    ADDED: Param Type Filters

--- JWT-with-Spring-Security/src/main/java/com/spring/jwt/dto/FilterDto.java
+++ JWT-with-Spring-Security/src/main/java/com/spring/jwt/dto/FilterDto.java
@@ -16,4 +16,15 @@ public class FilterDto {
     private String transmission;
     private String fuelType;
     private int year;
+
+    public FilterDto(Integer minPrice, Integer maxPrice, String area, String brand, String model, String transmission, String fuelType, Integer year) {
+        this.minPrice = minPrice;
+        this.maxPrice = maxPrice;
+        this.area = area;
+        this.brand = brand;
+        this.model = model;
+        this.transmission = transmission;
+        this.fuelType = fuelType;
+        this.year = year != null ? year : 0;
+    }
 }
