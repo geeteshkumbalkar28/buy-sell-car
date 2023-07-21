491ae43f95ed7f437a105db492282c48bd27be4d
tkraj888 <tusharck88@gmail.com>
2023-07-21 15:14:53 +0530

    ADDED: Getalli for PendingBooking

--- JWT-with-Spring-Security/src/main/java/com/spring/jwt/Interfaces/PendingBookingService.java
+++ JWT-with-Spring-Security/src/main/java/com/spring/jwt/Interfaces/PendingBookingService.java
@@ -1,10 +1,15 @@
 package com.spring.jwt.Interfaces;
 
+import com.spring.jwt.dto.CarDto;
 import com.spring.jwt.dto.PendingBookingDTO;
 import com.spring.jwt.entity.PendingBooking;
+
+import java.util.List;
 
 public interface PendingBookingService {
 
     PendingBooking savePendingBooking(PendingBookingDTO pendingBookingDTO);
+
+   List<PendingBookingDTO> getAllPendingBookingWithPage(int PageNo);
 
 }
---
+++ JWT-with-Spring-Security/src/main/java/com/spring/jwt/dto/ResponseAllPendingBookingDto.java
@@ -0,0 +1,15 @@
+package com.spring.jwt.dto;
+
+import lombok.Data;
+
+import java.util.List;
+@Data
+public class ResponseAllPendingBookingDto {
+
+    private String message;
+    private List<PendingBookingDTO> list;
+    private String exception;
+
+    public ResponseAllPendingBookingDto(String success) {
+    }
+}
